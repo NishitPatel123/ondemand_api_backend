5ee56375795fe1182cd767714fe4ae3a3a5be99c
Nishit <patelnishit2508@gmail.com>
2026-05-30 11:21:12 +0530

    index.js is updated

--- index.js
+++ index.js
@@ -64,8 +64,7 @@ app.use(cors({
     "http://localhost:3001",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://your-frontend.onrender.com",
-    "https://ondemand-api-backend-m1v5.onrender.com"
+    "https://ondemandservice-api-frontend.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
